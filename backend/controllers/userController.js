9dd27ed942974c3e41c66d63a41ba6310051ed23
Thanh Ngo <kamenriderlc@gmail.com>
2026-01-24 19:21:57 +0700

    add email verify and create sessionModel

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,11 @@
 import { User } from "../models/userModel.js"
-
-
-
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import "dotenv"
+import { vertifyEmail } from "../emailVerify/verifyEmail.js"
+import { Session } from "../models/sessionModel.js"
+import { sendOTPMail } from "../emailVerify/sendOTPMail.js"
+import router from "../routes/userRoute.js"
 export const register = async(req,res)=>{
     try {
         const {firstName, lastName, email, password} = req.body
@@ -18,12 +22,19 @@ export const register = async(req,res)=>{
                 message: 'User already exists'
             })
         }
+
+        const hashedPassword = await bcrypt.hash(password,10)
+
         const newUser = await User.create({
             firstName,
             lastName,
             email,
-            password
+            password: hashedPassword
         })
+
+        const token = jwt.sign({id:newUser._id},process.env.SECRET_KEY, {expiresIn: '10m'})
+        vertifyEmail(token,email) // send email here
+        newUser.token = token
         await newUser.save()
         return res.status(201).json({
             success: true,
@@ -33,6 +44,314 @@ export const register = async(req,res)=>{
     } catch (error) {
         res.status(500).json({
             success:false,
+            message: error.message
+        })
+    }
+}
+
+export const verify = async(req,res) =>{
+    try {
+        const authHeader = req.headers.authorization
+        if(!authHeader || !authHeader.startsWith("Bearer ")){
+            res.status(400).json({
+                success:false,
+                message: "Authorization token is missing or invalid"
+            })
+        }
+        const token = authHeader.split(" ")[1]//[bearer,  asdasdassd]
+        let decoded
+        try {
+            decoded = jwt.verify(token, process.env.SECRET_KEY)
+        } catch (error) {
+            if(error.name === "TokenExpiredError"){
+                return res.status(400).json({
+                    success:false,
+                    message: "The registration token has expired"
+                })
+            }
+            return res.status(400).json({
+                success:false,
+                message:"Token verification failed"
+            })
+        }
+        const user = await User.findById(decoded.id)
+        if(!user){
+            return res.status(400).json({
+                success:false,
+                message:"User not found"
+            })
+        }
+        user.token = null
+        user.isVerified = true
+        await user.save()
+        return res.status(200).json({
+            success:true,
+            message: "email verified successfully"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+        
+    }
+}
+
+export const reVerify = async(req,res) =>{
+    try {
+        const {email} = req.body
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({
+                success:false,
+                message: "user not found"
+            })
+        }
+        const token = jwt.sign({id:user._id},process.env.SECRET_KEY, {expiresIn: '10m'})
+        vertifyEmail(token,email) // send email here
+        user.token = token
+        await user.save()
+        return res.status(200).json({
+            success:true,
+            message: "Vertication email sent again successfully",
+            token: user.token
+        })
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message: error.message
+        })
+    }
+}
+
+export const login = async(req,res) =>{
+    try {
+        const {email,password} = req.body
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message: 'All fields are required'
+            })
+        }   
+
+        const exisitingUser = await User.findOne({email})
+        if(!exisitingUser){
+            return res.status(400).json({
+                success:false,
+                message: 'user not exists'
+            })
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, exisitingUser.password)
+        if(!isPasswordValid){
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid Credentials'
+            })
+        }
+        if(exisitingUser.isVerified === false){
+            return res.status(400).json({
+                success: false,
+                message: 'Verify your account than login'
+            })
+        }
+
+        const accessToken = jwt.sign({id:exisitingUser._id},process.env.SECRET_KEY, {expiresIn:"10d"})
+        const refreshToken = jwt.sign({id:exisitingUser._id},process.env.SECRET_KEY, {expiresIn:"30d"})
+
+        exisitingUser.isLoggedIn = true
+        await exisitingUser.save()
+
+        const existingSession = await Session.findOne({userId:exisitingUser._id})
+        if(existingSession){
+            await Session.deleteOne({userId: exisitingUser._id})
+        }
+
+        await Session.create({userId: exisitingUser._id})
+        return res.status(200).json({
+            success: true,
+            message: `Welcome back ${exisitingUser.firstName}`,
+            user:exisitingUser,
+            accessToken,
+            refreshToken
+        })
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message: error.message
+        })
+    }
+}
+
+export const logout = async(req,res) =>{
+    try {
+        const userId = req.id
+        await Session.deleteMany({userId:userId})
+        await User.findByIdAndUpdate(userId, {isLoggedIn:false})
+        return res.status(200).json({
+            success:true,
+            message: "User logged out successfully"
+        })
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message: error.message
+        }) 
+    }
+
+
+}
+
+export const forgotPassword = async(req,res)=>{
+    try{
+    const {email} = req.body
+    const user = await User.findOne({email})
+    if(!user){
+        return res.status(400).json({
+            success: false,
+            message: "User not found"
+        })
+    }
+    const otp = Math.floor(10000 + Math.random() * 900000).toString()
+    const otpExpiry = new Date(Date.now()+10*60*1000) //10min
+    user.otp = otp
+    user.otpExpiry = otpExpiry
+
+    await user.save()
+    await sendOTPMail(otp, email)
+    return res.status(200).json({
+        success: true,
+        message : 'Otp sent to email successfully'
+    })
+} catch(error){
+    return res.status(500).json({
+        success: false,
+        message : error.message
+    })
+}
+}
+
+export const verifyOTP = async(req,res)=>{
+    try {
+        const {otp} = req.body
+        const email = req.params.email
+        if(!otp){
+            return res.status(400).json({
+            success: false,
+            message: "OTP is required"
+        })
+        }
+        const user = await User.findOne({email})
+        if(!user){
+        return res.status(400).json({
+            success: false,
+            message: "User not found"
+        })
+        }
+        if(!user.otp || !user.otpExpiry){
+        return res.status(400).json({
+            success: false,
+            message: "Otp is not generated or already verified"
+        })
+        }
+        if(user.otpExpiry < new Date()){
+            return res.status(400).json({
+            success: false,
+            message: "Otp has expired please request a new one "
+        }) 
+        }
+        if(otp !== user.otp){
+            return res.status(400).json({
+            success: false,
+            message: "otp is invalid"
+        }) 
+        }
+        user.otp = null
+        user.otpExpiry = null
+        await user.save()
+        return res.status(200).json({
+            success: true,
+            message: "Otp verified successfully"
+        }) 
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        }) 
+    }
+}
+
+export const changePassword = async(_ ,res)=>{
+    try {
+        const {newPassword, confirmPassword} = req.body
+        const {email} = req.params
+        const user = await User.findOne({email})
+        if(!user){
+        return res.status(400).json({
+            success: false,
+            message: "User not found"
+        })
+        }
+        if(!newPassword || !confirmPassword){
+            return res.status(400).json({
+            success: false,
+            message: "All field is required"
+        })
+        }
+        if(newPassword !== confirmPassword){
+            return res.status(400).json({
+            success: false,
+            message: "password do not match"
+        })
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword,10)
+        user.password = hashedPassword
+        await user.save()
+        return res.status(200).json({
+            success: true,
+            message: "Password changed succesfully"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message : error.message
+        })
+    }
+}
+
+export const allUser = async(_,res) =>{
+    try {
+        const users = await User.find()
+        return res.status(200).json({
+            success: true,
+            users
+        }) 
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export const getUserById = async(req,res)=>{
+    try {
+        const {userId} = req.params
+        const user = await User.findById(userId).select("-password -otp -otpExpiry -token")
+        if(!user){
+          return res.status(400).json({
+            success: false,
+            message: "User not found"
+        }) 
+        }
+        res.status(200).json({
+            success: true,
+            user,
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
             message: error.message
         })
     }
